dbf056616045c5db1f7d6fde5727304fb4b0ce00
bion-slmn <bionsol25@gmail.com>
2023-05-13 00:47:47 -0700

    non interactive, split string and main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - it determine which mode of shell to use
+ *
+ * Return: 0 on sucess or 1 on fail
+ */
+int main(void)
+{
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		interactive(void);
+	}
+	else
+	{
+		non_interactive(void);
+	}
+
+	return (0);
+}
